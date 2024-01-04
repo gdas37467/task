130086d363adfff050353e0501971727946f3240
gdas <gdas37467@gmail.com>
2024-01-04 07:45:55 +0530

    db

--- mongoose.js
+++ mongoose.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost:27017/binbag', {
+mongoose.connect('mongodb+srv://gdas37467:qjI640ppwaVTuOox@binbag.nwsxnhl.mongodb.net/?retryWrites=true&w=majority', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
